271073e67c9f03732b211045f9332176ff868b51
Matt Benjamin <matt@cohortfs.com>
2014-11-21 09:41:38 -0800

    Add safe-sharing to buffer::list and buffer::ptr.
    Formerly known as "strong claim," this change introduces a new
    shareable() property to ceph::buffer::raw objects, which can be
    used to prevent unplanned sharing of buffers.
    
    The default value of shareable() is true, but derived classes may
    override this.  For example, Accelio message buffers may point to
    registered or pool-allocated memory which should not be held for
    long periods.
    
    Cloning of non-shareable buffers is now the unmarked case, so is
    performed automatically in the buffer::list copy constructor.
    Low-level operations should allow explicit volatile sharing,
    so copying a buffer::list:iterator or a buffer::ptr works as normal.
    
    For explicit sharing of "non-shareable" buffers, a new boolean is added to
    the public claim_* methods, and a buffer::list::share method is also
    added, to share an entire sequence.
    
    Signed-off-by: Vu Pham <vu@mellanox.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -164,6 +164,12 @@ static simple_spinlock_t buffer_debug_lock = SIMPLE_SPINLOCK_INITIALIZER;
     bool is_n_page_sized() {
       return (len & ~CEPH_PAGE_MASK) == 0;
     }
+    virtual bool is_shareable() {
+      // true if safe to reference/share the existing buffer copy
+      // false if it is not safe to share the buffer, e.g., due to special
+      // and/or registered memory that is scarce
+      return true;
+    }
     bool get_crc(const pair<size_t, size_t> &fromto,
 		 pair<uint32_t, uint32_t> *crc) const {
       Mutex::Locker l(crc_lock);
@@ -655,6 +661,18 @@ static simple_spinlock_t buffer_debug_lock = SIMPLE_SPINLOCK_INITIALIZER;
   buffer::raw *buffer::ptr::clone()
   {
     return _raw->clone();
+  }
+
+  buffer::ptr& buffer::ptr::clone_nonshareable() {
+    if (_raw && !_raw->is_shareable()) {
+      buffer::raw *tr = _raw;
+      _raw = tr->clone();
+      _raw->nref.set(1);
+      if (unlikely(tr->nref.dec() == 0)) {
+        delete tr;
+      }
+    }
+    return *this;
   }
 
   void buffer::ptr::swap(ptr& other)
@@ -1219,27 +1237,31 @@ void buffer::list::rebuild_page_aligned()
 }
 
   // sort-of-like-assignment-op
-  void buffer::list::claim(list& bl)
+  void buffer::list::claim(list& bl, bool clone_nonshareable)
   {
     // free my buffers
     clear();
-    claim_append(bl);
+    claim_append(bl, clone_nonshareable);
   }
 
-  void buffer::list::claim_append(list& bl)
+  void buffer::list::claim_append(list& bl, bool clone_nonshareable)
   {
     // steal the other guy's buffers
     _len += bl._len;
-    _buffers.splice( _buffers.end(), bl._buffers );
+    if (clone_nonshareable)
+       bl.clone_nonshareable();
+    _buffers.splice(_buffers.end(), bl._buffers );
     bl._len = 0;
     bl.last_p = bl.begin();
   }
 
-  void buffer::list::claim_prepend(list& bl)
+  void buffer::list::claim_prepend(list& bl, bool clone_nonshareable)
   {
     // steal the other guy's buffers
     _len += bl._len;
-    _buffers.splice( _buffers.begin(), bl._buffers );
+    if (clone_nonshareable)
+       bl.clone_nonshareable();
+    _buffers.splice(_buffers.begin(), bl._buffers );
     bl._len = 0;
     bl.last_p = bl.begin();
   }
