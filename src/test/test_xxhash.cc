633cdae00b2efa757bcb0e113cbea1dd4d2a7096
Matt Benjamin <mbenjamin@redhat.com>
2015-11-23 13:51:50 -0500

    xxhash: add timing code
    This isn't reliable for optimized builds, but yields ~40ns/hash on
    core i7 at -g3, -O0.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/test/test_xxhash.cc
@@ -0,0 +1,102 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 New Dream Network
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include <stdint.h>
+#include <tuple>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <random>
+#include "xxhash.h"
+
+#include "gtest/gtest.h"
+#include "common/ceph_argparse.h"
+#include "common/debug.h"
+#include "global/global_init.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+namespace {
+
+  int n_items = 1000000;
+  constexpr int seed = 8675309;
+
+  std::uniform_int_distribution<uint64_t> uint_dist;
+  std::mt19937 rng;
+
+  struct in6_addr_type
+  {
+    uint8_t _s6_addr[16];
+  };
+
+  struct ipv6_addr
+  {
+    union {
+      struct {
+	uint64_t low;
+	uint64_t hi;
+      } u64;
+      in6_addr_type uin6;
+    };
+    ipv6_addr(uint64_t low, uint64_t hi) {
+      u64.low = low;
+      u64.hi = hi;
+    }
+  };
+
+  std::vector<ipv6_addr> addrs;
+
+} /* namespace */
+
+TEST(XXHash, INIT) {
+  for (int ix = 0; ix < n_items; ++ix) {
+    addrs.push_back(ipv6_addr{uint_dist(rng), uint_dist(rng)});
+  }
+}
+
+TEST(XXHash, HASH1) {
+  uint64_t hk, sup = 0;
+  for (auto& addr : addrs) {
+    hk = XXH64(&addr, sizeof(ipv6_addr), seed);
+    /* naively (i.e., with cost) force evaluation of hk (defeat optimizers) */
+    if (hk > sup)
+      sup = hk;
+  }
+  std::cout << "hs = " << sup << std::endl;
+}
+
+TEST(XXHash, SHUTDOWN) {
+  // nothing
+}
+
+int main(int argc, char *argv[])
+{
+  string val;
+  vector<const char*> args;
+
+  argv_to_vec(argc, const_cast<const char**>(argv), args);
+  env_to_vec(args);
+
+  for (auto arg_iter = args.begin(); arg_iter != args.end();) {
+    if (ceph_argparse_witharg(args, arg_iter, &val, "--count",
+			      (char*) nullptr)) {
+      n_items = std::stoi(val);
+    } else {
+      ++arg_iter;
+    }
+  }
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
