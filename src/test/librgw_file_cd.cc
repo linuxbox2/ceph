e2186cf818ed21a6139c1d53d63e42a5d43e94e2
Matt Benjamin <mbenjamin@redhat.com>
2016-03-26 10:47:01 -0400

    Revert "rgw_file:  set initial owners 1"
    This reverts commit 04ef1d12590f8621a11c75d3b284e251d457e660.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/test/librgw_file_cd.cc
+++ src/test/librgw_file_cd.cc
@@ -33,9 +33,6 @@ namespace {
   string secret_key("");
   struct rgw_fs *fs = nullptr;
 
-  uint32_t owner_uid = 867;
-  uint32_t owner_gid = 5309;
-
   bool do_create = false;
   bool do_delete = false;
   bool do_multi = false;
@@ -66,8 +63,7 @@ TEST(LibRGW, CREATE_BUCKET) {
   if (do_create) {
     struct stat st;
     struct rgw_file_handle *fh;
-    int ret = rgw_mkdir(fs, fs->root_fh, bucket_name.c_str(),
-			owner_uid, owner_gid, 755, &st, &fh,
+    int ret = rgw_mkdir(fs, fs->root_fh, bucket_name.c_str(), 755, &st, &fh,
 			RGW_MKDIR_FLAG_NONE);
     ASSERT_EQ(ret, 0);
   }
@@ -89,8 +85,7 @@ TEST(LibRGW, CREATE_BUCKET_MULTI) {
     for (int ix = 0; ix < multi_cnt; ++ix) {
       string bn = bucket_name;
       bn += to_string(ix);
-      ret = rgw_mkdir(fs, fs->root_fh, bn.c_str(),
-		      owner_uid, owner_gid, 755, &st, &fh,
+      ret = rgw_mkdir(fs, fs->root_fh, bn.c_str(), 755, &st, &fh,
 		      RGW_MKDIR_FLAG_NONE);
       ASSERT_EQ(ret, 0);
       std::cout << "created: " << bn << std::endl;
