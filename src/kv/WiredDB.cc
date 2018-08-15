55b46a36035d9c94c9fef3691d505256a57934e9
Matt Benjamin <mbenjamin@redhat.com>
2018-08-15 14:30:28 -0400

    WiredDB interface fixes.
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/kv/WiredDB.cc
+++ src/kv/WiredDB.cc
@@ -45,6 +45,109 @@ void WiredDB::close()
   abort();
 } /* WiredDB::close() */
 
+int WiredDB::submit_transaction(KeyValueDB::Transaction t)
+{
+  abort();
+} /* WiredDB::submit_transaction */
+
+int WiredDB::submit_transaction_sync(KeyValueDB::Transaction t)
+{
+  abort();
+} /* WiredDB::submit_transaction_sync*/
+
+int WiredDB::get(
+  const string &prefix,
+  const std::set<string> &key,
+  std::map<string, bufferlist> *out)
+{
+  abort();
+} /* WiredDB::get 1 */
+
+int WiredDB::get(
+  const string &prefix,
+  const string &key,
+  bufferlist *out)
+{
+  abort();
+} /* WiredDB::get 2 */
+
+int WiredDB::get(
+  const string &prefix,
+  const char *key,
+  size_t keylen,
+  bufferlist *out)
+{
+  abort();
+} /* WiredDB::get 3 */
+
+KeyValueDB::Iterator WiredDB::get_iterator(const std::string& prefix)
+{
+  abort();
+} /* WiredDB::get_iterator */
+
+KeyValueDB::WholeSpaceIterator WiredDB::get_wholespace_iterator()
+{
+  abort();
+} /* WiredDB::get_wholespace_iterator */
+
 WiredDB::~WiredDB()
 {
 } /* WiredDB::~WiredDB() */
+
+WiredDB::WiredDBTransactionImpl::WiredDBTransactionImpl(WiredDB* db)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::WiredDBTransactionImpl */
+
+void WiredDB::WiredDBTransactionImpl::set(
+  const std::string& prefix,
+  const std::string& k,
+  const buffer::list& bl)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::set 1 */
+
+void WiredDB::WiredDBTransactionImpl::set(
+  const std::string& prefix,
+  const char* k,
+  size_t keylen,
+  const buffer::list& bl)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::set 2 */
+
+void WiredDB::WiredDBTransactionImpl::rmkey(
+  const std::string& prefix,
+  const std::string& k)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::rmkey 1 */
+
+void WiredDB::WiredDBTransactionImpl::rmkey(
+  const std::string& prefix,
+  const char* k,
+  size_t keylen)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::rmkey 2 */
+
+void WiredDB::WiredDBTransactionImpl::rm_single_key(
+  const std::string& prefix,
+  const std::string& k)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::rm_single_key */
+
+void WiredDB::WiredDBTransactionImpl::rmkeys_by_prefix(
+  const std::string& prefix)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::rmkeys_by_prefix */
+
+void WiredDB::WiredDBTransactionImpl::rm_range_keys(
+  const std::string& prefix,
+  const std::string& start,
+  const std::string& end)
+{
+  abort();
+} /* WiredDB::WiredDBTransactionImpl::rm_range_keys */
