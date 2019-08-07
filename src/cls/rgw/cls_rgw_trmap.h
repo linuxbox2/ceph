062324a13f07fc777d5e77d22e463bbee8f0169b
Matt Benjamin <mbenjamin@redhat.com>
2019-08-07 16:18:56 -0400

    initial
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/cls/rgw/cls_rgw_trmap.h
@@ -0,0 +1,10 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+#ifndef CLS_RGW_TRMAP_H
+#define CLS_RGW_TRMAP_H
+
+namespace rgw::cls::trmap {
+
+} /* namespace rgw::cls::trmap */
+
+#endif /* CLS_RGW_TRMAP_H */
