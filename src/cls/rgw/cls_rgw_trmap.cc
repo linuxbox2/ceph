b68c8a2356ae8785f637f4b9c83f90fc1bd0f5e1
Matt Benjamin <mbenjamin@redhat.com>
2019-08-08 11:31:29 -0400

    cont
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/cls/rgw/cls_rgw_trmap.cc
+++ src/cls/rgw/cls_rgw_trmap.cc
@@ -1,14 +1,16 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
+#include "include/types.h"
+
 #include <sqlite3.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <limits.h>
 
+#include "objclass/objclass.h"
 #include "cls_rgw_trmap.h"
-
 
 
 namespace rgw::cls::trmap {
@@ -21,6 +23,7 @@ namespace rgw::cls::trmap {
   {
   public:
     sqlite3_file base;
+    cls_method_context_t hctx;
     std::string name;
     /* XXX open flags? */
 
@@ -46,6 +49,24 @@ namespace rgw::cls::trmap {
       return SQLITE_OK;
     } /* trmap_sqlite_close */
 
+    static
+    int trmap_sqlite_read(sqlite3_file *_f, void *_buf, int len,
+			  sqlite_int64 off)
+    {
+      auto f = reinterpret_cast<TRMapFile*>(_f);
+
+      buffer::list bl;
+      bl.push_back(
+	buffer::create_static(len, static_cast<char*>(_buf)));
+      int ret = cls_cxx_read(f->hctx, off, len, &bl);
+      if ( ret < 0) {
+	return SQLITE_IOERR_READ;
+      }
+
+      return SQLITE_OK;
+    } /* trmap_sqlite_read */
+
+    
   } /* extern "C" */
 
 
