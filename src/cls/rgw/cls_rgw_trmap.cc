891e49906c01147ea3fef24a1ece6c0761b42d3d
Matt Benjamin <mbenjamin@redhat.com>
2019-08-08 10:59:06 -0400

    cont
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/cls/rgw/cls_rgw_trmap.cc
+++ src/cls/rgw/cls_rgw_trmap.cc
@@ -1,22 +1,58 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
+#include <sqlite3.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+
 #include "cls_rgw_trmap.h"
+
+
 
 namespace rgw::cls::trmap {
 
-  TRMap* get_instance(const std::string& path) 
+  /* SQLite3 VFS */
+  class TRMapFile;
+  class VFSCtx;
+
+  class TRMapFile
   {
-    return nullptr;
-  }
-  
+  public:
+    sqlite3_file base;
+    std::string name;
+    /* XXX open flags? */
+
+  }; /* TRMapFile */
+
+
+  class VFSCtx
+  {
+  public:
+    
+  }; /* VFSCtx */
+
+  static VFSCtx vCtx;
+
   extern "C" {
-    void get_muggle(void) 
+
+    /* VFS Ops */
+    static
+    int trmap_sqlite_close(sqlite3_file *_f)
     {
-      int x;
-    }
+      auto f = reinterpret_cast<TRMapFile*>(_f);
+      /* XXX do something with f */
+      return SQLITE_OK;
+    } /* trmap_sqlite_close */
 
   } /* extern "C" */
 
 
-} /* */
+  /* TRMap Singleton */  
+  TRMap* get_instance(const std::string& path) 
+  {
+    return nullptr;
+  }
+
+} /* namespace rgw::cls::trmap */
