7391b01d5e57f12f33af2f8ad7fcdae1a66cc032
Matt Benjamin <mbenjamin@redhat.com>
2020-01-18 16:59:20 -0500

    add files
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

---
+++ src/cls/rgw_bplus/bplus_client.h
@@ -0,0 +1,24 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ *
+ */
+
+#ifndef BPLUS_CLIENT_H
+#define BPLUS_CLIENT_H
+
+#include "bplus_types.h"
+
+namespace rgw::bplus::ondisk {
+
+
+} /* namespace */
+
+#endif /* BPLUS_CLIENT_H */
