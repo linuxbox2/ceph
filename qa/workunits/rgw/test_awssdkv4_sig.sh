d68e32e2ba0490c7d62286632b1da3aedfda3788
Ali Maredia <amaredia@redhat.com>
2024-02-05 11:52:38 -0500

    qa: enable test_awssdkv4_sig workunit to run in teuthology
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- qa/workunits/rgw/test_awssdkv4_sig.sh
+++ qa/workunits/rgw/test_awssdkv4_sig.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -e
 #
 # To run this test script with a cluster created via vstart.sh:
-# $PATH needs to be set for radosgw-admin and ceph_test_librgw executables.
-# $KEYRING need to be set as the path for a vstart clusters Ceph keyring.
+# $PATH needs to be set for radosgw-admin executables.
+# $CEPH_ROOT needs to be set to the path of the Ceph source code
+# $RGW_HTTP_ENDPOINT_URL needs to be set to the endpoint of the RGW
 #
 # Example when ceph source is cloned into $HOME and a vstart cluster is already running with a radosgw:
-# $ PATH=~/ceph/build/bin/:$PATH KEYRING=~/ceph/build/keyring ~/ceph/qa/workunits/rgw/test_awssdkv4_sig.sh
+# $ PATH=~/ceph/build/bin/:$PATH CEPH_ROOT=~/ceph RGW_HTTP_ENDPOINT=http://localhost:8000 ~/ceph/qa/workunits/rgw/test_awssdkv4_sig.sh
+#
+
+set -x
 
 if [ -z ${AWS_ACCESS_KEY_ID} ]
 then
@@ -17,29 +21,27 @@ then
        --secret $AWS_SECRET_ACCESS_KEY \
        --display-name "maven test user" \
        --email sigv4@example.com || echo "sigv4 maven user exists"
-
-    # keyring override for teuthology env
-    if [ -z ${KEYRING} ]
-    then
-      KEYRING="/etc/ceph/ceph.keyring"
-    fi
-    K="-k ${KEYRING}"
 fi
 
-# the required S3 access_key and secret_key are already exported above, but
-# we need to hook up the S3 endpoints
+if [ -z ${RGW_HTTP_ENDPOINT_URL} ]
+then
+  # TESTDIR and this block are meant for when this script is run in a teuthology environment
+  if [ -z ${TESTDIR} ]
+  then
+    echo "TESTDIR is not defined, cannot set RGW_HTTP_ENDPOINT_URL in teuthology"
+    exit
+  else
+    export RGW_HTTP_ENDPOINT_URL=$(cat ${TESTDIR}/url_file)
+  fi
+fi
 
-# the following are taken from
-
-# XXXX ok, so I think there should be only RGW_HTTP_ENDPOINT_URL and that
-# it may already be set before this script runs, though if we can figure it
-# out, it's ok if we set it...
-
-export RGW_HTTP_ENDPOINT_URL="http://localhost:80"
-#export RGW_HTTPS_ENDPOINT_URL="https://localhost:443"
-
-# rgw/test_awssdkv4_sig.sh
-pushd jcksum
+if [ -z ${CEPH_ROOT} ]
+then
+  echo "CEPH_ROOT is not defined"
+  exit
+else
+  cd $CEPH_ROOT/qa/workunits/rgw/jcksum
+fi
 
 ./mvnw clean package
 ./mvnw test -Dtest=PutObjects
